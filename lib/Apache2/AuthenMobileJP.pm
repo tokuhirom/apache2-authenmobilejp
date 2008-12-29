c82198dec4d4e9995b677d83c97debb7d94833cd
Tokuhiro Matsuno <tokuhirom@gmail.com>
2008-12-29 09:07:42 +0900

    initial commit

---
+++ lib/Apache2/AuthenMobileJP.pm
@@ -0,0 +1,33 @@
+package Apache2::AuthenMobileJP;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Apache2::AuthenMobileJP -
+
+=head1 SYNOPSIS
+
+  use Apache2::AuthenMobileJP;
+
+=head1 DESCRIPTION
+
+Apache2::AuthenMobileJP is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom jsdfkla gmail fsadkjl comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
